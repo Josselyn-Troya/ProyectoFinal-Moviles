ba3c680393aaa0d7228069597bb6a2d3fbeb2f91
Josselyn-Troya <josselyn.troya@epn.edu.ec>
2021-09-07 10:20:26 -0500

    menu

--- app/src/main/java/com/example/socialmedia/activities/HomeActivity.java
+++ app/src/main/java/com/example/socialmedia/activities/HomeActivity.java
@@ -1,16 +1,66 @@
 package com.example.socialmedia.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.socialmedia.R;
+import com.example.socialmedia.fragments.ChatsFragment;
+import com.example.socialmedia.fragments.FiltersFragment;
+import com.example.socialmedia.fragments.HomeFragment;
+import com.example.socialmedia.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(new HomeFragment());
     }
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    if (item.getItemId() == R.id.itemHome) {
+                        // FRAGMENT HOME
+                        openFragment(new HomeFragment());
+                    }
+                    else if (item.getItemId() == R.id.itemChats) {
+                        // FRAGMENT CHATS
+                        openFragment(new ChatsFragment());
+
+                    }
+                    else if (item.getItemId() == R.id.itemFilters) {
+                        // FRAGMENT FILTROS
+                        openFragment(new FiltersFragment());
+
+                    }
+                    else if (item.getItemId() == R.id.itemProfile) {
+                        // FRAGMENT PROFILE
+                        openFragment(new ProfileFragment());
+                    }
+                    return true;
+                }
+            };
 }
---
+++ app/src/main/java/com/example/socialmedia/fragments/FiltersFragment.java
@@ -0,0 +1,31 @@
+package com.example.socialmedia.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.socialmedia.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FiltersFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FiltersFragment extends Fragment {
+
+    public FiltersFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_filters, container, false);
+    }
+}
